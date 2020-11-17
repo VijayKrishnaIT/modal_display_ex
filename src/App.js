bce6c5194d9841b80905823b0d4ca7deb83fbc63
vijay <nimmagaddav9@gmail.com>
2020-11-16 21:18:21 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Modal from "react-modal";
+Modal.setAppElement("#root");
 
 function App() {
+  const [openModal, setOpenModal] = useState(false);
+  const [count, setCount] = useState(0);
+  const toggleModal = () => setOpenModal((prevVal) => !prevVal);
+  const resetCount = () => setCount(0);
+  const afterOpenModal = () => setCount((prevVal) => prevVal + 1);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button type="button" className="open" onClick={toggleModal}>
+        Open
+      </button>
+      <button type="button" className="reset" onClick={resetCount}>
+        Reset
+      </button>
+
+      <Modal
+        isOpen={openModal}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={toggleModal}
+        shouldCloseOnOverlayClick={true}
+      >
+        {`Modal opened ${count} time${count === 1 ? "" : "s"}`}
+        <div className="close">
+          <button type="button" onClick={toggleModal}>
+            Close Modal
+          </button>
+        </div>
+      </Modal>
     </div>
   );
 }
